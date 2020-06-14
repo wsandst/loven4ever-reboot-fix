04304fa45e53ae46499cb4797a087b3eab2b4b69
williamsandst <williamsandstrom99@hotmail.com>
2020-06-14 14:22:14 +0200

    Improved functionality

--- script.py
+++ script.py
@@ -1,11 +1,12 @@
 #pip3 install requests
+#cron-tab: */2 * * * * sudo python3 home/pi/Desktop/script.py
 
 import os
 
 import requests
 from datetime import datetime
 
-URL = 'https://loven4ever.com/admin'
+URL = 'https://localhost/admin'
 ERROR_STATUS_CODE = 204
 COMMAND_ON_ERROR = "sudo service apache2 restart"
 
