e2e213fc8794c76d391c2140e74a5801e2b0074b
williamsandst <williamsandstrom99@hotmail.com>
2020-06-22 19:28:18 +0200

    Improved functionality

--- script.py
+++ script.py
@@ -6,18 +6,23 @@ import os
 import requests
 from datetime import datetime
 
-URL = "https://localhost/admin"
-#URL = "https://loven4ever.com/admin"
-ERROR_STATUS_CODE = 204
+URL = "https://www.loven4ever.com/admin"
 COMMAND_ON_ERROR = "sudo service apache2 restart"
 
+def log(message: str):
+    with open("log.txt", "a") as myfile:
+        myfile.write(datetime.now().__str__() + message + "\n")
+        os.system(COMMAND_ON_ERROR)
+
 def main():
-    r = requests.get(URL)
-    print(r.status_code)
-    if (r.status_code == ERROR_STATUS_CODE):
-        with open("log.txt", "a") as myfile:
-            myfile.write(datetime.now().__str__() + " Fixed \n")
-            os.system(COMMAND_ON_ERROR)
+    try:
+        r = requests.get(URL)
+        print("Website working, doing nothing")
+        log("Alright")
+    except requests.exceptions.ConnectionError:
+        print("Website down, rebooting Apache2")
+        log("Fixed")
+        os.system(COMMAND_ON_ERROR)
 
 if __name__ == "__main__":
     main()
